9a7c1730a79dfa1031e4eb67d3824c6e52c8e679
shailja1317 <25113fw007@glbitm.ac.in>
2026-06-07 22:25:32 +0530

    Day-7 is uploaded

---
+++ Day-7/Sumofdigits.c
@@ -0,0 +1,18 @@
+//write a program to find the sum of digits of a number using recursion
+#include <stdio.h>
+
+int sumOfDigits(int n) {
+    if (n == 0) {
+        return 0;
+    } else {
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Sum of digits of %d is %d\n", num, sumOfDigits(num));
+    return 0;
+}
