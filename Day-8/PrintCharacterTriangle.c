be596a2c612dd079edcc9d466b7353399b8b0bcf
shailja1317 <25113fw007@glbitm.ac.in>
2026-06-08 11:49:31 +0530

    Day-8 is uploaded

---
+++ Day-8/PrintCharacterTriangle.c
@@ -0,0 +1,19 @@
+//Write a program to print character triangle.
+#include<stdio.h>
+
+int main()
+{
+    int n ,i ,j;
+
+    printf("enter number of rows:");
+    scanf("%d", &n);
+
+    for(i=1 ;i<=n; i++)
+{
+for(j=1;j<=i;j++){
+    printf("%c",j+64);
+}
+printf("\n");}
+
+    return 0;
+}
---
+++ Day-8/PrintHalfPyramidPattern.c
@@ -0,0 +1,18 @@
+//Write a program to print a half pyramid pattern.
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
