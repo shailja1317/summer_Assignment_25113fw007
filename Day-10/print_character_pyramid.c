2d89af4840fc76d2f1cbef203e9a5e081db9949c
shailja1317 <25113fw007@glbitm.ac.in>
2026-06-10 14:29:35 +0530

    Day-10 is uploaded

---
+++ Day-10/print_character_pyramid.c
@@ -0,0 +1,21 @@
+//write a program to print character pyramid
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 0; i < 5; i++) {
+
+        for(j = 0; j <= i; j++) {
+            printf("%c", 'A' + j);
+        }
+
+        for(j = i - 1; j >= 0; j--) {
+            printf("%c", 'A' + j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
